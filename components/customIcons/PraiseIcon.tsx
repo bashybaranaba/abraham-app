3f9b5cbe3d60f65c8121b083e613f5f6d0f6ed1c
bashybaranaba <bashybaranaba@gmail.com>
2024-09-21 00:22:20 +0300

    feat: add cusotm praise icon

--- components/customIcons/PraiseIcon.tsx
+++ components/customIcons/PraiseIcon.tsx
@@ -12,13 +12,12 @@ const PraiseIcon: React.FC<PraiseIconProps> = ({ className, ...props }) => (
     viewBox="0 0 48 24"
     fill="none"
     stroke="currentColor"
-    strokeWidth="2" // Reduced stroke width for slender hands
+    strokeWidth="2"
     strokeLinecap="round"
     strokeLinejoin="round"
     className={className}
     {...props}
   >
-    {/* Left hand (close to the center) */}
     <g transform="translate(24, 0) scale(-1, 1)">
       <path d="M18 11V6a2 2 0 0 0-2-2a2 2 0 0 0-2 2" />
       <path d="M14 10V4a2 2 0 0 0-2-2a2 2 0 0 0-2 2v2" />
@@ -26,7 +25,6 @@ const PraiseIcon: React.FC<PraiseIconProps> = ({ className, ...props }) => (
       <path d="M18 8a2 2 0 1 1 4 0v6a8 8 0 0 1-8 8h-2c-2.8 0-4.5-.86-5.99-2.34l-3.6-3.6a2 2 0 0 1 2.83-2.82L7 15" />
     </g>
 
-    {/* Right hand (close to the center) */}
     <g transform="translate(24.5, 0)">
       <path d="M18 11V6a2 2 0 0 0-2-2a2 2 0 0 0-2 2" />
       <path d="M14 10V4a2 2 0 0 0-2-2a2 2 0 0 0-2 2v2" />
